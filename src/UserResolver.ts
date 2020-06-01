eda6222461d23858fc7f5c811e1ec32529c079f6
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-06-01 15:08:35 -0700

    Adds initial routes, and validation for registration

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,9 +1,67 @@
-import {Resolver, Query} from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { User } from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
+
 
 @Resolver()
 export class UserResolver {
-    @Query(() => String) 
+    @Query(() => String)
     hello() {
         return 'hi!';
+    };
+
+    @Query(() => [User])
+    users() {
+        return User.find();
+    };
+
+    @Mutation(() => Boolean)
+    async register(
+        @Arg('email') email: string,
+        @Arg('password') password: string,
+    ) {
+
+        const hashedPassword = await hash(password, 12);
+
+        try {
+            await User.insert({
+                email,
+                password: hashedPassword,
+            });
+        } catch (err) {
+            console.log(err);
+            return false;
+        };
+
+        return true;
+    };
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('email') email: string,
+        @Arg('password') password: string,
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error("could not find user");
+        }
+
+        const valid = await compare(password, user.password)
+
+        if (!valid) {
+            throw new Error("password not valid")
+        }
+
+        return {
+            accessToken: sign({ userId: user.id }, "12121212", { expiresIn: "15m" })
+        };
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 import "reflect-metadata";
 import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
+import { createConnection } from "typeorm";
 
 
 (async () => {
-    const app = express(); 
+    const app = express();
     app.get("/", (_, res) => {
         res.send("hello")
     });
+
+    await createConnection();
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [UserResolver]
         })
-    });
+    }); 
 
-    apolloServer.applyMiddleware({app}); 
-    
+    apolloServer.applyMiddleware({ app });
+
     app.listen(4000, () => {
         console.log("express server started");
-    })
-})(); 
+    });
+})();
 
 
-// createConnection().then(async connection => {
+// createConnection().then(async connection => { 
 
 //     console.log("Inserting a new user into the database...");
 //     const user = new User();
