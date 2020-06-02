94d6ff5c7d64c2f71dc84a1de9807141bb9238a9
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-06-01 17:10:12 -0700

    formats

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,69 +1,73 @@
-import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql';
-import { hash, compare } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
-import { User } from './entity/User';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { User } from "./entity/User";
 import "./utils/config.ts";
 
 @ObjectType()
 class LoginResponse {
-    @Field()
-    accessToken: string
+  @Field()
+  accessToken: string;
 }
-
 
 @Resolver()
 export class UserResolver {
-    @Query(() => String)
-    hello() {
-        return 'hi!';
-    };
+  @Query(() => String)
+  hello() {
+    return "hi!";
+  }
 
-    @Query(() => [User])
-    users() {
-        return User.find();
-    };
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
 
-    @Mutation(() => Boolean)
-    async register(
-        @Arg('email') email: string,
-        @Arg('password') password: string,
-    ) {
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
 
-        const hashedPassword = await hash(password, 12);
-
-        try {
-            await User.insert({
-                email,
-                password: hashedPassword,
-            });
-        } catch (err) {
-            console.log(err);
-            return false;
-        };
-
-        return true;
-    };
-
-    @Mutation(() => LoginResponse)
-    async login(
-        @Arg('email') email: string,
-        @Arg('password') password: string,
-    ): Promise<LoginResponse> {
-        const user = await User.findOne({ where: { email } });
-
-        if (!user) {
-            throw new Error("could not find user");
-        }
-
-        const valid = await compare(password, user.password)
-
-        if (!valid) {
-            throw new Error("password not valid")
-        }
-
-
-        return {
-            accessToken: sign({ userId: user.id }, "a" , { expiresIn: "15m" })
-        };
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
     }
+
+    return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("password not valid");
+    }
+
+    return {
+      accessToken: sign({ userId: user.id }, "a secret", { expiresIn: "15m" }),
+    };
+  }
 }
